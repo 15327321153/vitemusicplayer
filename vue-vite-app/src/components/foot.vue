1f482fe2e52538068e3e142a77744a96998eec6c
15327321153 <1286273584@qq.com>
2023-05-04 22:58:49 +0800

    播放功能

--- vue-vite-app/src/components/foot.vue
+++ vue-vite-app/src/components/foot.vue
@@ -1,147 +1,141 @@
 <template>
   <div class="footbody">
-    <el-slider v-model="data.value1" class="slider" :min="data.min" :max="data.max" />
+    <el-slider
+      v-model="data.value1"
+      class="slider"
+      :min="data.min"
+      :max="data.max"
+    />
     <div class="fa">
-      
       <div class="img">
-        <img src="../assets/logo.jpg" alt="">
+        <img src="../assets/logo.jpg" alt="" />
       </div>
       <div class="title">
         <span class="name">认真的雪</span>
         <span class="airname">薛之谦</span>
       </div>
-      <div class="time">
-        00：00/03：24      
-      </div>
+      <div class="time">00：00/03：24</div>
       <div class="icons">
-        <svg class="icon" aria-hidden="true"  >
-          <use
-            xlink:href="#icon-shangyiqu"
-          ></use>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-shangyiqu"></use>
         </svg>
 
-        
         <svg class="icon" aria-hidden="true" v-if="data.sope" @click="bofang">
-          <use
-            xlink:href="#icon-mv-red"
-          ></use>
+          <use xlink:href="#icon-mv-red"></use>
         </svg>
 
-        <svg class="icon" aria-hidden="true"  v-else @click="bofang">
-          <use
-            xlink:href="#icon-sope"
-          ></use>
+        <svg class="icon" aria-hidden="true" v-else @click="bofang">
+          <use xlink:href="#icon-sope"></use>
         </svg>
-
 
         <svg class="icon" aria-hidden="true">
-          <use
-            xlink:href="#icon-xiayiqu"
-          ></use>
+          <use xlink:href="#icon-xiayiqu"></use>
         </svg>
-      
       </div>
 
       <div class="shengying">
-
-          <svg class="icon" aria-hidden="true"  v-if="data.jinyin.type" @click="jinying">
-          <use
-            xlink:href="#icon-yinliang"
-          ></use>
+        <svg
+          class="icon"
+          aria-hidden="true"
+          v-if="data.jinyin.type"
+          @click="jinying"
+        >
+          <use xlink:href="#icon-yinliang"></use>
         </svg>
-        <svg class="icon" aria-hidden="true" v-else @click="jinying" >
-          <use
-            xlink:href="#icon-jinyin"
-          ></use>
+        <svg class="icon" aria-hidden="true" v-else @click="jinying">
+          <use xlink:href="#icon-jinyin"></use>
         </svg>
 
-
-          <el-slider v-model="data.value1" class="slider" :min="data.min" :max="data.max" />
-
+        <el-slider
+          v-model="data.shengying"
+          class="slider"
+          :min="0"
+          :max="100"
+          @input="changeshengying"
+        />
       </div>
       <div class="button">
-        <svg class="icon" aria-hidden="true"  >
-          <use
-            xlink:href="#icon-xunhuanbofang"
-          ></use>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-xunhuanbofang"></use>
         </svg>
-        <svg class="icon" aria-hidden="true"  >
-          <use
-            xlink:href="#icon-geciweidianji"
-          ></use>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-geciweidianji"></use>
         </svg>
-        <svg class="icon" aria-hidden="true"  >
-          <use
-            xlink:href="#icon-list"
-          ></use>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-list"></use>
         </svg>
-        <svg class="icon" aria-hidden="true"  >
-          <use
-            xlink:href="#icon-SmallWindow"
-          ></use>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-SmallWindow"></use>
         </svg>
-        <svg class="icon" aria-hidden="true"  >
-          <use
-            xlink:href="#icon-16gl-M"
-          ></use>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-16gl-M"></use>
         </svg>
       </div>
-
     </div>
   </div>
 </template>
 
-<script lang='ts' setup>
-import { reactive } from "vue";
-
-
-
+<script lang="ts" setup>
+import { computed, reactive } from "vue";
 
 const data = reactive({
   value1: 0,
+  shengying:100,
   max: 100,
   min: 0,
-  sope:true,
-  jinyin:{
-    type:true,
-    value:0
+  sope: true,
+  jinyin: {
+    type: true,
+    value: 0,
   },
-  mp3: new Audio(" https://music.163.com/song/media/outer/url?id=164305610.mp3",
-)
-})
+  mp3: new Audio(
+    "http://m7.music.126.net/20230504012403/761149d5bdb32a21feef13e15cdf79eb/ymusic/0fd6/4f65/43ed/a8772889f38dfcb91c04da915b301617.mp3"
+  ),
+});
 
 //静音功能
-const jinying = ()=>{
-if(data.jinyin.type){
-  data.jinyin.value = data.value1
-  data.value1 = 0
-}else{
-  data.value1 =data.jinyin.value
-}
-data.jinyin.type = !data.jinyin.type
-}
+const jinying = () => {
+  if (data.jinyin.type) {
+    data.jinyin.value = data.shengying;
+    data.shengying = 0;
+    
+  } else {
+    data.shengying = data.jinyin.value;
+  }
+  data.jinyin.type = !data.jinyin.type;
+};
 
-const bofang = ()=>{
-  //播放音乐
-  let mp3 = new Audio('http://m7.music.126.net/20230504012403/761149d5bdb32a21feef13e15cdf79eb/ymusic/0fd6/4f65/43ed/a8772889f38dfcb91c04da915b301617.mp3') // 创建音频对象
-mp3.loop = true
-mp3.play() // 播放
-
-  // data.sope = !data.sope
+const bofang = () => {
+  if(data.sope){
+    data.mp3.play();
+  }else{
+    data.mp3.pause();
+  }
+  data.sope = !data.sope
+};
+// data.jinyin.type = computed(()=>{
+//   if(data.shengying>0){
+//     return true
+//   }else{
+//     return false
+//   }
+// })
+//音量调节
+const changeshengying = ()=>{
+  data.mp3.volume = data.shengying/100
 }
-
 
 const setvalue = () => {
   setTimeout(() => {
-    data.value1 = data.value1 + 1
+    data.value1 = data.value1 + 1;
     if (data.value1 < 1800) {
-      setvalue()
+      setvalue();
     }
-  }, 1000)
-}
+  }, 1000);
+};
 </script>
 
-<style lang='less' scoped>
+<style lang="less" scoped>
 .footbody {
   background-color: #ffffff;
 
@@ -150,69 +144,67 @@ const setvalue = () => {
   .slider {
     height: 5px;
   }
-  .fa{
+  .fa {
     display: flex;
     flex-flow: row nowrap;
     justify-content: space-between;
     width: 1300px;
     margin: 0 auto;
-    .img{
+    .img {
       width: 48px;
       height: 48px;
       margin: 10px 0;
 
-      img{
+      img {
         width: 100%;
         border-radius: 4px;
       }
     }
-    .title{
+    .title {
       display: flex;
       flex-flow: column nowrap;
       justify-content: space-between;
       margin: 15px 0;
-      .name{
+      .name {
         font-size: 12px;
         font-weight: 700;
       }
-      .airname{
+      .airname {
         font-size: 12px;
         color: #999;
       }
     }
-    .time{
+    .time {
       line-height: 60px;
       font-size: 14px;
       color: #606266;
-
     }
-    .icons{
+    .icons {
       margin: 12px 0;
-      .icon{
+      .icon {
         cursor: pointer;
         width: 50px;
         margin: 0 25px;
       }
     }
-    .shengying{
+    .shengying {
       width: 180px;
       display: flex;
-      .icon{
+      .icon {
         margin: 12px 10px;
         cursor: pointer;
       }
-      .slider{
+      .slider {
         margin: 25px 10px;
       }
     }
-    .button{
+    .button {
       margin: 12px 0;
-      .icon{
+      .icon {
         margin: 0 10px;
         cursor: pointer;
       }
     }
-
   }
 }
 </style>
