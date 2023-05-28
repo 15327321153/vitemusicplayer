f8dc90ea13961412dce313173a6a5d593329a10b
15327321153 <1286273584@qq.com>
2023-05-28 22:16:26 +0800

    歌单页面

--- vue-vite-app/src/view/song/index.vue
+++ vue-vite-app/src/view/song/index.vue
@@ -1,11 +1,11 @@
 <template>
-    歌曲
+    <div>
+        歌单页面 
+    </div>
 </template>
 
-<script lang='ts' setup>
+<script lang="ts" setup>
 
 </script>
 
-<style lang='less' scoped>
-
-</style>
+<style lang="less" scoped></style>
